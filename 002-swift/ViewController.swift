c912b03ad0cd3ca28c5f3d4dcef9a06c5ad2e670
young <858222866@qq.com>
2017-08-25 12:05:28 +0800

    update

--- 002-swift/ViewController.swift
+++ 002-swift/ViewController.swift
@@ -22,7 +22,7 @@ class ViewController: UIViewController {
             }
         }
         
-        navigationItem.title = "closures"
+        navigationItem.title = "using closures"
     }
     
     
