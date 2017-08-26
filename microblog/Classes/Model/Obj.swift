70c3b19a1a62981f4558934bf58894ed8802554f
young <858222866@qq.com>
2017-08-27 00:20:41 +0800

    add test files

---
+++ microblog/Classes/Model/Obj.swift
@@ -0,0 +1,13 @@
+//
+//  Obj.swift
+//  microblog
+//
+//  Created by cy on 2017/8/27.
+//  Copyright © 2017年 Aisino. All rights reserved.
+//
+
+import UIKit
+
+class Obj: NSObject {
+
+}
---
+++ microblog/Classes/ViewModel/VM.swift
@@ -0,0 +1,13 @@
+//
+//  VM.swift
+//  microblog
+//
+//  Created by cy on 2017/8/27.
+//  Copyright © 2017年 Aisino. All rights reserved.
+//
+
+import UIKit
+
+class VM: NSObject {
+
+}
